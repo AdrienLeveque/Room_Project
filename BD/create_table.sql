d57a1479f70b12f6a41df2ce72fc0c4c6ba29833
raduCatalin <radu.catalin81@gmail.com>
2016-05-02 11:59:57 +0200

    version 1

--- BD/create_table.sql
+++ BD/create_table.sql
@@ -1,9 +1,15 @@
-drop table if exists Statut;
-drop table if exists Client;
-drop table if exists Clef;
-drop table if exists Type_salle;
-drop table if exists Salle;
+drop table if exists Materiel_reservee;
+drop table if exists Reservation;
 drop table if exists Materiel;
+drop table if exists Salle_clef;
+drop table if exists Salle;
+drop table if exists Type_salle;
+drop table if exists Clef;
+drop table if exists Employe;
+drop table if exists Type_employe;
+drop table if exists Client;
+drop table if exists Statut;
+
 
 CREATE TABLE IF NOT EXISTS Statut(
 id_statut int(11) not null AUTO_INCREMENT,
@@ -28,6 +34,26 @@ constraint PK_client PRIMARY KEY (id_client),
 constraint Fk_client_statut FOREIGN KEY (id_statut_client) REFERENCES Statut (id_statut)
 );
 
+CREATE TABLE IF NOT EXISTS Type_employe(
+id_type_employe int(11) not null AUTO_INCREMENT,
+libelle_type_employe varchar(20) not null,
+reduction_type_employe float(2,2) not null,
+constraint PK_materiel_reservee PRIMARY KEY (id_type_employe)
+);
+
+CREATE TABLE IF NOT EXISTS Employe(
+id_employe int(11) not null AUTO_INCREMENT,
+nom_employe varchar(20) not null,
+prenom_employe varchar(20) not null,
+adresse_employe varchar(40) not null,
+telephone_employe varchar(20), 
+courriel_employe varchar(30) not null,
+mdp_employe varchar(20) not null,
+id_type_employe int(11) not null,
+constraint PK_employe PRIMARY KEY (id_employe),
+constraint Fk_employe_typeEmploye FOREIGN KEY (id_type_employe) REFERENCES Type_employe (id_type_employe)
+);
+
 CREATE TABLE IF NOT EXISTS Clef(
 id_clef int(11) not null AUTO_INCREMENT,
 emruntee_clef boolean not null,
@@ -50,10 +76,10 @@ capacite_salle int(11) not null,
 gardien_salle boolean not null,
 tarif_salle float(5,2) not null,
 caution_salle float(5,2) not null,
-id_type_de_salle int(11) not null,
+id_type_salle int(11) not null,
+id_employe_salle int(11),
 constraint PK_salle PRIMARY KEY (id_salle),
-constraint Fk_salle_type_salle FOREIGN KEY (id_type_de_salle) REFERENCES Type_salle (id_type_salle),
-constraint Fk_salle_clef FOREIGN KEY (id_clef_salle) REFERENCES Clef (id_clef)
+constraint Fk_salle_type_salle FOREIGN KEY (id_type_salle) REFERENCES Type_salle (id_type_salle)
 );
 
 CREATE TABLE IF NOT EXISTS Salle_clef(
@@ -67,51 +93,34 @@ constraint Fk_salle_clef_clef FOREIGN KEY (id_clef) REFERENCES Clef (id_clef)
 
 CREATE TABLE IF NOT EXISTS Materiel(
 id_materiel int(11) not null AUTO_INCREMENT,
-libelle_maerial varchar(20) not null,
-caution_material float(5,2) not null,
-quantite_materiel int(11) not null,
+libelle_maeriel varchar(20) not null,
+caution_materiel float(5,2) not null,
+abime_materiel boolean not null,
 id_salle int(11),
 constraint PK_materiel PRIMARY KEY (id_materiel),
-constraint Fk_materiel_salle FOREIGN KEY (id_salle) REFERENCES Clef (id_salle)
-);
-
-CREATE TABLE IF NOT EXISTS Salle_materiel(
-id_salle_materiel int(11) not null AUTO_INCREMENT,
-id_salle int(11) not null,
-id_materiel int(11) not null,
-quantite_material int not null,
-constraint PK_salle_materiel PRIMARY KEY (id_salle_materiel),
-constraint Fk_salleMateriel_salle FOREIGN KEY (id_salle) REFERENCES Salle (id_salle),
-constraint Fk_salleMateriel_materiel FOREIGN KEY (id_materiel) REFERENCES Materiel (id_materiel)
-);
-
-CREATE TABLE IF NOT EXISTS Depot(
-id_depot int(11) not null AUTO_INCREMENT,
-libelle_depot varchar(20) not null,
-adresse varchar(20) not null,
-constraint PK_depot PRIMARY KEY (id_depot)
-);
-
-CREATE TABLE IF NOT EXISTS Ligne_materiel(
-id_ligne_materiel int(11) not null AUTO_INCREMENT,
-quantite_ligne_material int(11) not null,
-id_materiel_dansLigne int(11) not null,
-constraint PK_ligne_materiel PRIMARY KEY (id_ligne_materiel),
-constraint Fk_ligneMateriel_materiel FOREIGN KEY (id_materiel_dansLigne) REFERENCES Materiel (id_materiel)
-);
-
-CREATE TABLE IF NOT EXISTS Materiel_abime(
-id_materiel_abime int(11) not null AUTO_INCREMENT,
-quantite_materiel_abime int(11) not null,
-id_materiel int(11) not null,
-constraint PK_materiel_abime PRIMARY KEY (id_materiel_abime),
-constraint Fk_materielAbime_materiel FOREIGN KEY (id_materiel) REFERENCES Materiel (id_materiel)
+constraint Fk_materiel_salle FOREIGN KEY (id_salle) REFERENCES Salle (id_salle)
 );
 
 CREATE TABLE IF NOT EXISTS Reservation(
-id_reservation int(11) not null,
+id_reservation int(11) not null AUTO_INCREMENT,
 date_debut date not null,
 date_fin date not null,
 etat_lieux_effectue boolean not null,
-constraint PK_reservation PRIMARY KEY (id_reservation)
+id_client_reservation int(11) not null,
+id_employe_reservation int(11) not null, 
+id_salle_reservation int(11) not null,
+constraint PK_reservation PRIMARY KEY (id_reservation),
+constraint Fk_reservation_client FOREIGN KEY (id_client_reservation) REFERENCES Client (id_client),
+constraint Fk_reservation_salle FOREIGN KEY (id_salle_reservation) REFERENCES Salle (id_salle),
+constraint Fk_reservation_employe FOREIGN KEY (id_employe_reservation) REFERENCES Employe (id_employe)
 );
+
+CREATE TABLE IF NOT EXISTS Materiel_reservee(
+id_materiel_reservee int(11) not null AUTO_INCREMENT,
+id_reservation int(11) not null,
+id_materiel int(11) not null,
+constraint PK_materiel_reservee PRIMARY KEY (id_materiel_reservee),
+constraint Fk_materiel_reservee_materiel FOREIGN KEY (id_materiel) REFERENCES Materiel (id_materiel),
+constraint Fk_materiel_reservee_reservation FOREIGN KEY (id_reservation) REFERENCES Reservation (id_reservation)
+);
+
