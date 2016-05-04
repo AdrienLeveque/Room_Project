7f5aa1b656a4dbf6281762f4f0d0aa3bee374d6f
raduCatalin <radu.catalin81@gmail.com>
2016-05-04 14:23:47 +0200

    Ajout des pages (Comment ça marche.php)

---
+++ Projet_Web/Comment_ca_marche.php
@@ -0,0 +1,62 @@
+<?php include("head.php") ?>
+<body>	
+<!-- Titre -->
+	 <div class="container">
+	 	  <div class="col-md-offset-3">
+			<h1><strong> Reservation de votre salle en ligne</strong></h1>
+		</div>
+	</div>
+	</br>
+
+<!-- Barre de navigation -->
+		<div class="container">
+		    <nav class="navbar navbar-default">
+		      	<div class="navbar-header">
+  					<a class="navbar-brand" href="#">Utilisateur non enregistré: </a>
+  				</div>
+		        <div class="container-fluid">
+		          <ul class="nav navbar-nav">
+		            <li> <a href="Accueil.php">Accueil</a> </li>
+		            <li> <a href="connexion.php">Connexion</a></li>
+		            <li> <a href="Inscription.php">Inscription</a> </li>
+		            <li class="active"> <a href="Comment_ca_marche.php">Comment ça marche</a> </li>
+		            <li> <a href="contact.php">Contact</a> </li>
+		          </ul>
+		        </div>
+		     </nav>
+		</div>
+
+		<div class="container">
+			<header class="page-header">
+				<h1>Les types de location proposés sont :</h1>
+			</header>
+			<h4>1.	Location des locaux de moindre envergure - loués à titre gratuit</h4> 
+			<div class="col-md-12 text-justify ">
+				<blockquote class="small">	
+					<p>En ce qui concerne les locaux de moindre envergure, leur planning est assez rigide dans la mesure où il s’agit essentiellement de permanences ou de locaux occupés en permanence par les associations.
+					</p>
+					<p>En cas de demande pour ces salles, on consulte leur planning et selon la disponibilité on la loue mais à titre gratuit, les particuliers ne peuvent pas louer cette catégorie de salle, la priorité est donnée aux permanences.
+					</p>
+				</blockquote>
+				<br>
+				<h4>2.	Location des salles payantes - avec particularités </h4>
+				<blockquote class="small">
+					<p>Au sein de la mairie, il y a uniquement quatre salles payantes avec certaines particularités. Ces salles payantes ne le sont pas toujours, dans la mesure où il existe un certain nombre de critères dont il faut tenir compte pour savoir si la salle sera payante ou non. 
+					</p>
+					<p>La salle est non payante si : 
+					</p>
+					<ul>
+						<li>le pétitionnaire est une association à caractère non politique, il peut obtenir la gratuité de la salle à condition que la manifestation soit sans recette de toute nature.</li>
+						<li>en cas des manifestations organisées par les syndicats ouvriers, des partis politiques, anciens combattants, invalides du travail, donneurs de sang, ou bien organisés par le comité d’action culturelle et touristique ou son parrainage, ou enfin organisés par le COS (Comité d’œuvres Sociales) de la mairie et le COS des services publics locaux.</li>
+					</ul>
+				</blockquote>
+				<h4>3.	Location du matériel</h4>
+				<blockquote class="small">
+					<p>Il faut savoir qu’il est possible de disposer de matériel, or tout type de matériel ne peut pas être loué par tout le monde.
+					</p>
+					<p>Si des dégradations se produisent sur du matériel loué, un devis est demandé au service technique pour émettre une facture de dégradation.
+					</p>
+				</blockquote>
+			</div>
+		</div>
+</body>	
