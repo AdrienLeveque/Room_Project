00834d7b428b402940894805bf7a8813258b9004
gaetancousinÃ <cousin.gaetan@laposte.net>
2016-05-05 20:58:16 +0200

    ajout de la migration de la table materiel_reserve

---
+++ room_project/database/migrations/2016_05_05_000010_materiel_reserve.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MaterielReserve extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materiel_reserve', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('abime');
+            $table->integer('reservation_id')->unsigned();
+            $table->integer('salle_id')->unsigned();
+            $table->integer('materiel_id')->unsigned();
+        });
+
+
+        Schema::table('materiel_reserve', function($table) {    
+            $table->foreign('reservation_id')->references('id')->on('reservation');
+            $table->foreign('salle_id')->references('id')->on('salle');
+            $table->foreign('materiel_id')->references('id')->on('materiel'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('materiel_reserve');
+    }
+}
