6d8bfb9928631e321f1e01226f1b5ad9a48443c8
raduCatalin <radu.catalin81@gmail.com>
2016-05-07 10:41:29 +0200

    Ajout de la vue: accueil.blade.php

---
+++ room_project/resources/views/accueil.blade.php
@@ -0,0 +1,60 @@
+@extends('template')
+
+@section('contenu')	
+<!-- Titre -->
+	 <div class="container">
+	 	  <div class="col-md-offset-3">
+			<h1><strong> Reservation de votre salle en ligne</strong></h1>
+		</div>
+	</div>
+	</br>
+
+<!-- Barre de navigation -->
+		<div class="container">
+		    <nav class="navbar navbar-default">
+		      	<div class="navbar-header">
+  					<a class="navbar-brand" href="#">Utilisateur non enregistré: </a>
+  				</div>
+		        <div class="container-fluid">
+		          <ul class="nav navbar-nav">
+		            <li class="active"> <a href="accueil">Accueil</a> </li>
+		            <li> <a href="connexion">Connexion</a></li>
+		            <li> <a href="inscription">Inscription</a> </li>
+		            <li> <a href="comment_ca_marche">Comment ça marche</a> </li>
+		            <li> <a href="contact">Contact</a> </li>
+		          </ul>
+		        </div>
+		     </nav>
+		</div>
+		
+		<div class="container">
+			<section class="row">
+				<div class="col-xs-offset-2 col-xs-8 col-md-offset-2 col-md-8">
+				<br>
+					<legend class="text-center padding">LOCATION DE SALLES À ORSAY</legend>
+					<p class="text-justify">Tous les grands événements sont liés à des lieux d'émotions exceptionnels... Au travers des salles Parisiennes remarquables, votre événement prendra toute sa grandeur.
+					Pour un séminaire, un banquet, un anniversaire, un mariage, une conférence, un défilé ou tout autre événement professionel, vous trouverez dans ces salles, des ambiances différentes (loft New-Yorkais, au bord l'eau, cabaret ...) , il ne vous reste plus qu'à choisir l'ambiance qui correspond le plus à votre événement. Salles atypiques à Orsay pour des événements réussis : Pour organiser des événements réussis et inoubliables, qu’ils soient professionnels ou privés, Lieux d’émotions met à votre disposition les salles des plus prestigieuses à Orsay.
+					</p>
+				</div>
+			</section>
+			<br>
+			<br>
+			<section class="row">
+				<div class="col-md-12">
+					<div class="col-xs-4 col-md-4"><img src="cd ../cd ../images/images.jpe" class="img-thumbnail img-responsive center-block" alt="Responsive image" style="margin-bottom: 2%;"></div>
+					<div class="col-xs-4 col-md-4"><img src="images/images1.jpe" class="img-thumbnail img-responsive center-block" alt="Responsive image" style="margin-bottom: 2%;"></div>
+					<div class="col-xs-4 col-md-4"><img src="images/images2.jpe" class="img-thumbnail img-responsive center-block" alt="Responsive image" style="margin-bottom: 2%;"></div>
+					<div class="col-xs-4 col-md-4"><img src="images/images3.jpe" class="img-thumbnail img-responsive center-block" alt="Responsive image" style="margin-bottom: 2%;"></div>
+					<div class="col-xs-4 col-md-4"><img src="images/images5.jpe" class="img-thumbnail img-responsive center-block" alt="Responsive image" style="margin-bottom: 2%;"></div>
+					<div class="col-xs-4 col-md-4"><img src="images/images6.jpe" class="img-thumbnail img-responsive center-block" alt="Responsive image" style="margin-bottom: 2%;"></div>
+					<div class="col-xs-4 col-md-4"><img src="images/images7.jpe" class="img-thumbnail img-responsive center-block" alt="Responsive image" style="margin-bottom: 2%;"></div>
+					<div class="col-xs-4 col-md-4"><img src="images/images11.jpe" class="img-thumbnail img-responsive center-block" alt="Responsive image" style="margin-bottom: 2%;"></div>
+					<div class="col-xs-4 col-md-4"><img src="images/images12.jpe" class="img-thumbnail img-responsive center-block" alt="Responsive image" style="margin-bottom: 2%;"></div>
+					<div class="col-xs-4 col-md-4"><img src="images/images13.jpe" class="img-thumbnail img-responsive center-block" alt="Responsive image" style="margin-bottom: 2%;"></div>
+					<div class="col-xs-4 col-md-4"><img src="images/images8.jpe" class="img-thumbnail img-responsive center-block" alt="Responsive image" style="margin-bottom: 2%;"></div>
+					<div class="col-xs-4 col-md-4"><img src="images/images9.jpe" class="img-thumbnail img-responsive center-block" alt="Responsive image" style="margin-bottom: 2%;"></div>
+				</div>
+			</section>
+		</div>
+		
+@stop	
