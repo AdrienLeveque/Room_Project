2c6c830ece57b880fdfc681766e45df7563ea224
AdrienLeveque <adrien.leveque@yahoo.fr>
2016-05-13 12:02:36 +0200

    Mise a jour de la table client_consulter_salles

--- room_project/resources/views/client_consulter_salles.blade.php
+++ room_project/resources/views/client_consulter_salles.blade.php
@@ -1,32 +1,20 @@
-@extends('template')
+@extends('layouts.app')
 
-@section('contenu')
+@section('accueil')  @endsection
+
+@section('a_propos')  @endsection  
+
+@section('client_consulter_salles') class="active" @endsection
+
+@section('contact')  @endsection
+
+@section('connexion')  @endsection
+
+@section('inscription')  @endsection
+
+@section('deconnection')  @endsection
+@section('content')
 <!-- Titre -->
-	 <div class="container">
-	 	  <div class="col-md-offset-3">
-			<h1><strong> Reservation de votre salle en ligne</strong></h1>
-		</div>
-	</div>
-	</br>
-
-<!-- Barre de navigation -->
-			<div class="container">
-		    <nav class="navbar navbar-default">
-		      	<div class="navbar-header">
-  					<a class="navbar-brand" href="#">Client: </a>
-  				</div>
-		        <div class="container-fluid">
-		          <ul class="nav navbar-nav">
-		            <li> <a href="client_accueil">Accueil</a> </li>
-		            <li class="active"> <a href="client_consulter_salles">Consulter les salles</a></li>
-		            <li> <a href="client_reservation">Mes reservations</a> </li>
-		            <li> <a href="client_compte">Mon compte</a> </li>
-		            <li> <a href="client_contact">Contact</a> </li>
-		          </ul>
-		        </div>
-		     </nav>
-		</div>
-
 		<div class="container">
 			<form action="reservation_paiement.php" method="post">
 				<div class="col-md-12">
@@ -42,10 +30,21 @@
 					$idSalle=$infosSalle->id;
 					?>
 					<div class="col-xs-4 col-md-4">
-						<a href="client_reservation_salles?id=<?php echo $idSalle; ?>"> <img src="images/<?php echo $pathSalle ?>" class="img-thumbnail img-responsive center-block" alt="Responsive image" style="margin-bottom: 2%;"></a>
-						<label><?php if(!is_null($nomSalle)) echo $nomSalle ?></label>
-						<label><?php if(!is_null($capaciteSalle))echo $capaciteSalle ?></label>
-						<label><?php if(!is_null($cautionSalle))echo $cautionSalle ?></label>
+						<div class="col-md-12">
+							<a href="client_reservation_salles?id=<?php echo $idSalle; ?>"> <img src="images/<?php echo $pathSalle ?>" class="img-thumbnail img-responsive center-block" alt="Responsive image" style="margin-bottom: 2%;"></a>
+							<label>
+								<div class="col-md-4">Nom: </div> 
+								<?php if(!is_null($nomSalle)) echo $nomSalle ?>
+							</label>
+							<label>
+								<div class="col-md-4">Capacite: </div>
+								<?php if(!is_null($capaciteSalle))echo $capaciteSalle ?>
+							</label>
+							<label>
+								<div class="col-md-4">Caution :</div>
+									<?php if(!is_null($cautionSalle))echo $cautionSalle ?>
+							</label>
+						</div>
 					</div>
 				<?php
 					}
